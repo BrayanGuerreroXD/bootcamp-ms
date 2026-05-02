1d5868c0f581193a0a0f3a0a0b4c7f8f042ff71a
Brayan Guerrero <brayanstewartguor@gmail.com>
2026-05-01 21:15:33 -0500

    refactor: remove GetFullBootcampService bean from UseCasesConfig

--- applications/app-service/src/main/java/co/com/bootcamp/config/UseCasesConfig.java
+++ applications/app-service/src/main/java/co/com/bootcamp/config/UseCasesConfig.java
@@ -1,16 +1,10 @@
 package co.com.bootcamp.config;
 
-import co.com.bootcamp.usecase.getfullbootcamp.GetFullBootcampService;
-import co.com.bootcamp.usecase.getfullbootcamp.GetFullBootcampUseCase;
 import co.com.bootcamp.model.bootcamp.gateways.BootcampCapacityRepository;
 import co.com.bootcamp.model.bootcamp.gateways.BootcampPeopleRepository;
 import co.com.bootcamp.model.bootcamp.gateways.BootcampRepository;
-import co.com.bootcamp.model.capacitycatalog.gateways.CapacityCatalogRepository;
 import co.com.bootcamp.model.event.gateways.EventGateway;
-import co.com.bootcamp.model.technologycapacitycatalog.gateways.TechnologyCapacityCatalogRepository;
 import co.com.bootcamp.usecase.gettechnologycapacitycatalog.GetTechnologyCapacityCatalogService;
-import co.com.bootcamp.usecase.gettechnologycapacitycatalog.GetTechnologyCapacityCatalogUseCase;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -22,19 +16,4 @@ import org.springframework.context.annotation.FilterType;
         },
         useDefaultFilters = false)
 public class UseCasesConfig {
-
-    @Bean
-    public GetFullBootcampService getFullBootcampService(
-            BootcampRepository bootcampRepository,
-            BootcampCapacityRepository bootcampCapacityRepository,
-            BootcampPeopleRepository bootcampPeopleRepository,
-            GetTechnologyCapacityCatalogService getTechnologyCapacityCatalogService,
-            EventGateway eventGateway) {
-        return new GetFullBootcampUseCase(
-                bootcampRepository,
-                bootcampCapacityRepository,
-                bootcampPeopleRepository,
-                getTechnologyCapacityCatalogService,
-                eventGateway);
-    }
 }
