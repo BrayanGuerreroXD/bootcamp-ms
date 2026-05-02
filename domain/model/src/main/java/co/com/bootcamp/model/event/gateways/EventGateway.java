8b295346bf0c1eea1218ee39de8b9ece9f36eee2
Brayan Guerrero <brayanstewartguor@gmail.com>
2026-05-01 20:17:17 -0500

    feat: add publishBootcampReportSync to EventGateway

--- domain/model/src/main/java/co/com/bootcamp/model/event/gateways/EventGateway.java
+++ domain/model/src/main/java/co/com/bootcamp/model/event/gateways/EventGateway.java
@@ -1,5 +1,6 @@
 package co.com.bootcamp.model.event.gateways;
 
+import co.com.bootcamp.model.event.BootcampEvent;
 import co.com.bootcamp.model.event.CapacityBootcampSyncEvent;
 import co.com.bootcamp.model.event.BootcampDeleteMatchEvent;
 import reactor.core.publisher.Mono;
@@ -7,4 +8,5 @@ import reactor.core.publisher.Mono;
 public interface EventGateway {
     Mono<Void> publishBootcampDeleteMatch(BootcampDeleteMatchEvent event);
     Mono<Void> publishCapacitiesBootcampsMatch(CapacityBootcampSyncEvent event);
+    Mono<Void> publishBootcampReportSync(BootcampEvent event);
 }
