ae03e8f5ab46c6e5b967866f854d975c1ef7a6f8
Brayan Guerrero <brayanstewartguor@gmail.com>
2026-05-01 20:21:34 -0500

    feat: register GetFullBootcampService in UseCasesConfig

--- applications/app-service/src/main/java/co/com/bootcamp/config/UseCasesConfig.java
+++ applications/app-service/src/main/java/co/com/bootcamp/config/UseCasesConfig.java
@@ -1,5 +1,12 @@
 package co.com.bootcamp.config;
 
+import co.com.bootcamp.usecase.getfullbootcamp.GetFullBootcampService;
+import co.com.bootcamp.usecase.getfullbootcamp.GetFullBootcampUseCase;
+import co.com.bootcamp.repository.bootcamp.BootcampCapacityRepository;
+import co.com.bootcamp.repository.bootcamp.BootcampPeopleRepository;
+import co.com.bootcamp.repository.bootcamp.BootcampRepository;
+import co.com.bootcamp.gateway.EventGateway;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -11,4 +18,17 @@ import org.springframework.context.annotation.FilterType;
         },
         useDefaultFilters = false)
 public class UseCasesConfig {
+
+    @Bean
+    public GetFullBootcampService getFullBootcampService(
+            BootcampRepository bootcampRepository,
+            BootcampCapacityRepository bootcampCapacityRepository,
+            BootcampPeopleRepository bootcampPeopleRepository,
+            EventGateway eventGateway) {
+        return new GetFullBootcampUseCase(
+                bootcampRepository,
+                bootcampCapacityRepository,
+                bootcampPeopleRepository,
+                eventGateway);
+    }
 }
